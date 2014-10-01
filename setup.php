c15f85b3de8f18e2951d1805f2032d9fd510b733
Blaise BARRE <blaise.barre@dixinfor.com>
2014-10-01 11:44:52 +0200

    Modification du setup

--- setup.php
+++ setup.php
@@ -37,54 +37,53 @@ function plugin_init_notimportedemail() {
 function plugin_item_add_notimportedemail_notimportedemail($parm) {
     global $DB ; 
     
-    if($parm->getField("reason") == 1) {
-        $contenu = "<html style='font-family:Arial;font-size:14px;'><body><p>Bonjour, <br /><br />GLPI n'a pu traiter une demande reçu par email, voici les informations : <br /><br />
-                                <ul><li>Raison du refus : Courriel absent. Import impossible ; </li>
-                                    <li>Email du courriel : ".$parm->getField("from")." ; </li>
-                                    <li>Sujet du mail : ".$parm->getField("subject")." ; </li>
-                                    <li>Collecteur concerné : ".$parm->getField("to")." ; </li>
-                                    <li>Date de réception : ".$parm->getField("date").".</li></ul><br />" ; 
+    $contenu = "<html style='font-family:Arial;font-size:14px;'><body><p>Bonjour, <br /><br />GLPI n'a pu traiter une demande reçu par email, voici les informations : <br /><br />
+                            <ul><li>Raison du refus : Courriel absent. Import impossible ; </li>
+                                <li>Email du courriel : ".$parm->getField("from")." ; </li>
+                                <li>Sujet du mail : ".$parm->getField("subject")." ; </li>
+                                <li>Collecteur concerné : ".$parm->getField("to")." ; </li>
+                                <li>Date de réception : ".$parm->getField("date").".</li>
+                                <li>Raison : ".NotImportedEmail::getReason($parm->getField("reason"))."</li></ul><br />" ; 
         
-        $entity = new Entity ; 
-        $entity->getFromDB("0") ;
-        $contenu .= nl2br($entity->getField("mailing_signature"))."</p></body></html>"  ; 
+    $entity = new Entity ; 
+    $entity->getFromDB("0") ;
+    $contenu .= nl2br($entity->getField("mailing_signature"))."</p></body></html>"  ; 
         
-        $sujet = htmlspecialchars_decode(utf8_decode("[DIXINFOR] Erreur de traitement d'un email reçu")) ; 
+    $sujet = htmlspecialchars_decode(utf8_decode("[DIXINFOR] Erreur de traitement d'un email reçu")) ; 
         
-        //-----------------------------------------------
-        //GENERE LA FRONTIERE DU MAIL ENTRE TEXTE ET HTML
-        //-----------------------------------------------
+    //-----------------------------------------------
+    //GENERE LA FRONTIERE DU MAIL ENTRE TEXTE ET HTML
+    //-----------------------------------------------
 
-        $frontiere = '-----=' . md5(uniqid(mt_rand()));
+    $frontiere = '-----=' . md5(uniqid(mt_rand()));
 
-        //-----------------------------------------------
-        //HEADERS DU MAIL
-        //-----------------------------------------------
+    //-----------------------------------------------
+    //HEADERS DU MAIL
+    //-----------------------------------------------
 
-        $headers = 'From: "DIXINFOR " <support@dixinfor.com>'."\n";
-        $headers .= 'MIME-Version: 1.0'."\n";
-        $headers .= 'Content-Type: multipart/alternative; boundary="'.$frontiere.'"';
+    $headers = 'From: "DIXINFOR " <support@dixinfor.com>'."\n";
+    $headers .= 'MIME-Version: 1.0'."\n";
+    $headers .= 'Content-Type: multipart/alternative; boundary="'.$frontiere.'"';
 
-        //-----------------------------------------------
-        //MESSAGE HTML
-        //-----------------------------------------------
-        $message = '' ;
-        $message .= '--'.$frontiere."\n";
+    //-----------------------------------------------
+    //MESSAGE HTML
+    //-----------------------------------------------
+    $message = '' ;
+    $message .= '--'.$frontiere."\n";
 
-        $message .= 'Content-Type: text/html; charset="iso-8859-1"'."\n";
-        $message .= 'Content-Transfer-Encoding: 8bit'."\n\n";
-        $message .= htmlspecialchars_decode(utf8_decode($contenu))."\n\n";
+    $message .= 'Content-Type: text/html; charset="iso-8859-1"'."\n";
+    $message .= 'Content-Transfer-Encoding: 8bit'."\n\n";
+    $message .= htmlspecialchars_decode(utf8_decode($contenu))."\n\n";
 
-        $message .= '--'.$frontiere."\n";
+    $message .= '--'.$frontiere."\n";
 
 
-        $query_select_contact = "SELECT users_id FROM glpi_plugin_notimportedemail_users
-        WHERE  notification = '1'" ; 
-        foreach ($DB->request($query_select_contact) as $data) { 
-            $user = new User ; 
-            $user->getFromDB($data['users_id']) ; 
-            mail($user->getDefaultEmail(),$sujet,$message, $headers) ;
-        }
+    $query_select_contact = "SELECT users_id FROM glpi_plugin_notimportedemail_users
+    WHERE  notification = '1'" ; 
+    foreach ($DB->request($query_select_contact) as $data) { 
+        $user = new User ; 
+        $user->getFromDB($data['users_id']) ; 
+        mail($user->getDefaultEmail(),$sujet,$message, $headers) ;
     }
     return true ; 
 }
